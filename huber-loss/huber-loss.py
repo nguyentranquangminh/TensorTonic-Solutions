fd9c378cba05be44fd78b4367a043635357d9fff
Nguyen Tran Quang Minh <84921371+nguyentranquangminh@users.noreply.github.com>
2026-04-13 16:44:11 +0700

    Update solution for huber-loss

--- huber-loss/huber-loss.py
+++ huber-loss/huber-loss.py
@@ -5,7 +5,7 @@ def huber_loss(y_true, y_pred, delta=1.0):
     Compute Huber Loss for regression.
     """
     # Write code here
-    residual = np.array(np.array(y_true) - np.array(y_pred))
+    residual = np.array(y_true) - np.array(y_pred)
     lower = np.sum((1/2)*(residual[np.where(np.abs(residual) <= delta)]**2))
     higher = np.sum(delta * (np.abs(residual[np.where(np.abs(residual) > delta)]) - (1/2)*delta))
     return (higher + lower) / len(y_true)
